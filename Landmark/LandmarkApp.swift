c91c669ae1df6e75f42708f1df1dfb606336f8d0
Viktor Nyberg <viktor.nyberg00@gmail.com>
2025-08-12 15:42:42 +0200

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Viktor Nyberg on 2025-08-12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
