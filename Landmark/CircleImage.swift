c472ad79acf898cf948410c273feafd6ba0d2521
Viktor Nyberg <viktor.nyberg00@gmail.com>
2025-08-12 17:02:58 +0200

    map, img

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 270)
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+
+#Preview {
+    CircleImage()
+}
+
