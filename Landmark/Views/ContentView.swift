1f761dfecd5da8a3e5d3fba7d2feaad7809168ba
Viktor Nyberg <viktor.nyberg00@gmail.com>
2025-08-12 17:54:50 +0200

    Landmark list

---
+++ Landmark/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by Viktor Nyberg on 2025-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
